00d825b779ae232fe602e69c6c332c1c2c382e2f
ThomasKluiters <tmkluiters@student.tudelft.nl>
2017-04-03 22:34:17 +0200

    Add Iterator support for all model classes

--- src/main/java/nl/schiphol/api/models/flights/FlightCodeShares.java
+++ src/main/java/nl/schiphol/api/models/flights/FlightCodeShares.java
@@ -2,15 +2,24 @@ package nl.schiphol.api.models.flights;
 
 import lombok.Data;
 
+import javax.annotation.Nonnull;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by
  * Thomas on 22-3-2017.
  */
 @Data
-public class FlightCodeShares {
+public class FlightCodeShares implements Iterable<String> {
 
-    private String[] codeshares;
+    private List<String> codeshares;
 
     FlightCodeShares() { }
 
+    @Override
+    @Nonnull
+    public Iterator<String> iterator() {
+        return getCodeshares().iterator();
+    }
 }
--- src/main/java/nl/schiphol/api/models/flights/FlightTransferPositions.java
+++ src/main/java/nl/schiphol/api/models/flights/FlightTransferPositions.java
@@ -2,14 +2,23 @@ package nl.schiphol.api.models.flights;
 
 import lombok.Data;
 
+import javax.annotation.Nonnull;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by Thomas on 22-3-2017.
  */
 @Data
-public class FlightTransferPositions {
+public class FlightTransferPositions implements Iterable<Integer> {
 
-    private int[] transferPositions;
+    private List<Integer> transferPositions;
 
     FlightTransferPositions() { }
 
+    @Override
+    @Nonnull
+    public Iterator<Integer> iterator() {
+        return transferPositions.iterator();
+    }
 }
